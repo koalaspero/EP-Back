de6abc9ea762a9cb6c07373543a6cea3ea9ecf7b
Carlos Meneses <cmeneses@espol.edu.ec>
2023-12-17 00:32:02 -0500

    Carlos: Backend diagnostico

--- routes/results.py
+++ routes/results.py
@@ -3,17 +3,22 @@ from sqlalchemy import desc
 from config.db import conn
 from models.results import Result as ResultTB
 from schemas.results import Result as Result
+from models.user import users
+from models.medical_observation import MedicalObservation as MedicalDB
 
 result = APIRouter()
+
 
 @result.get("/results", tags=["results"])
 def get_results():
     result = conn.execute(ResultTB.select()).fetchall()
-
     list = []
-
     for row in result:
-
+        doctor = conn.execute(users.select().where(users.c.id == row[6])).first()
+        observation= conn.execute(MedicalDB.select().where(MedicalDB.c.result == row[0])).all()
+        observation_list = []
+        for obs in observation:
+            observation_list.append(obs[1])
         dict = {
             "id": str(row[0]),
             "fecha": row[1],
@@ -21,43 +26,44 @@ def get_results():
             "resultext": row[3],
             "result": row[4],
             "probability": row[5],
-            "doctor": str(row[6]),
+            "doctor": doctor.username,
+            "observation": observation_list,
         }
 
         list.append(dict)
 
     return {"data": list}
 
+
 @result.get("/results/{id}", tags=["results"])
 def get_result(id):
-
     db_object = conn.execute(ResultTB.select().where(ResultTB.c.id == id)).first()
 
     if db_object is None:
         return {"error": "Result not found"}  # or any appropriate response
-    
+
     dict = {
-            "id": str(db_object[0]),
-            "fecha": db_object[1],
-            "hasParkinson": bool(db_object[2]),
-            "resultext": db_object[3],
-            "result": db_object[4],
-            "probability": db_object[5],
-            "doctor": str(db_object[6]),
-        }
+        "id": str(db_object[0]),
+        "fecha": db_object[1],
+        "hasParkinson": bool(db_object[2]),
+        "resultext": db_object[3],
+        "result": db_object[4],
+        "probability": db_object[5],
+        "doctor": str(db_object[6]),
+    }
 
     return {"data": dict}
 
+
 @result.post("/results", tags=["results"])
 def create_result(res: dict):
-
     new_res = {
-        "fecha": res['fecha'],
-        "hasParkinson": res['hasParkinson'],
-        "resultext": res['resultext'],
-        "result": bytes(res['result'].values()),
-        "probability": res['probability'],
-        "doctor": res['doctor']
+        "fecha": res["fecha"],
+        "hasParkinson": res["hasParkinson"],
+        "resultext": res["resultext"],
+        "result": bytes(res["result"].values()),
+        "probability": res["probability"],
+        "doctor": res["doctor"],
     }
 
     result = conn.execute(ResultTB.insert().values(new_res))
